44279453676d2828409199b615ccf5f1668623b9
seregahom <seregahom2013@gmail.com>
2019-08-30 11:24:11 +0700

    add

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1 class="red">My Text</h1>
+<?php echo '<p>Мой текст!</p>'; ?>
+<?php include "test.php" ?>
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,7 @@
+<?php
+function myFirstFunction($name, $surname){
+    echo "<h1> Привет, $name $surname!</h1>";
+}
+
+    myFirstFunction('Иван','Иванов');
+
